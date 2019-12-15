0472f14ec6bb3a28102d6526d932bf3d32995604
AmilaBahor <49698674+AmilaBahor@users.noreply.github.com>
2019-12-15 23:58:26 +0100

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  document.getElementById("rd1").innerHTML = "Footsteps.";
+}
+function myFunction1() {
+  document.getElementById("rd2").innerHTML = "The living room.";
+}
+function myFunction2() {
+  document.getElementById("rd3").innerHTML = "A piano.";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
